7b8bb88904cdd1ecac8b0af56633bdb649db2c4d
michaelbobkov <michaelbobkov1@gmail.com>
2023-11-17 02:38:39 +0100

    Added form

---
+++ add.php
@@ -0,0 +1,42 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Ninja Pizza</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style type="text/css">
+        .brand{
+            background: #cb3455 !important;
+        }
+        .brand-text{
+            color: #39dd55 !important;
+        }
+    </style>
+</head>
+<body class="grey lighten-4">
+<?php include('templates/header.php'); ?>
+
+<section class="container grey-text">
+    <h3 class="center"></h3>
+    <form>
+        <label for="email">Your Email:</label>
+        <input type="text" id="email">
+        <label for="title">Your Title:</label>
+        <input type="text" id="title">
+        <label for="requirements">Requirements:</label>
+        <input type="text" id="requirements">
+        <div class="center">
+            <input type="submit" name="submit" value="Submit" class="btn brand z-depth-0">
+        </div>
+    </form>
+</section>
+
+
+<?php include('templates/footer.php'); ?>
+</body>
+</html>

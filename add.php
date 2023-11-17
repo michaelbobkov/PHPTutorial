e1594175eba6438eb05e5b41924d39bedd32698f
michaelbobkov <michaelbobkov1@gmail.com>
2023-11-17 02:58:32 +0100

    Added response handling

--- add.php
+++ add.php
@@ -1,41 +1,31 @@
 <?php
-
+if (isset($_POST['submit'])){
+    echo htmlentities($_POST['email']);
+    echo htmlentities($_POST['title']);
+    echo htmlentities($_POST['requirements']);
+}
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Ninja Pizza</title>
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <style type="text/css">
-        .brand{
-            background: #cb3455 !important;
-        }
-        .brand-text{
-            color: #39dd55 !important;
-        }
-        form{
-            max-width: 500px;
-            margin: 20px auto;
-        }
-    </style>
+    <?php include('templates/head.php'); ?>
 </head>
 <body class="grey lighten-4">
 <?php include('templates/header.php'); ?>
 
 <section class="container grey-text">
     <h3 class="center"></h3>
-    <form>
+    <form class="white" action="add.php" method="post">
         <label for="email">Your Email:</label>
-        <input type="text" id="email">
+        <input type="text" id="email" name="email">
         <label for="title">Title:</label>
-        <input type="text" id="title">
+        <input type="text" id="title" name="title">
         <label for="requirements">Requirements:</label>
-        <input type="text" id="requirements">
+        <input type="text" id="requirements" name="requirements">
         <div class="center">
-            <input type="submit" name="submit" value="Submit" class="btn brand z-depth-0">
+            <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
         </div>
     </form>
 </section>

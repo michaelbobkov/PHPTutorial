a2973066abfc17721db28fccbfe41fdd3e1cd47d
michaelbobkov <michaelbobkov1@gmail.com>
2023-11-18 23:38:47 +0100

    Retrieve data from database

--- index.php
+++ index.php
@@ -6,6 +6,12 @@ $connect = mysqli_connect('localhost', 'michael', '1234', 'test');
 if(!$connect){
     echo 'Connection error: '.mysqli_connect_error();
 }
+$sql = 'SELECT title, requirements, id FROM products';
+$result = mysqli_query($connect, $sql);
+$products = mysqli_fetch_all($result, MYSQLI_ASSOC);
+mysqli_free_result($result);
+mysqli_close($connect);
+print_r($products);
 ?>
 
 <!DOCTYPE html>

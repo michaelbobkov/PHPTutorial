962971c5133a07c6acefc5f1e4d1a9cd8f853870
michaelbobkov <michaelbobkov1@gmail.com>
2023-11-19 13:56:19 +0100

    Rearranged files and added info on the index page

--- add.php
+++ add.php
@@ -1,71 +1,94 @@
 <?php
-$title = $email = $requirements ='';
-$errors = array('email'=>'', 'title'=>'', 'requirements'=>'');
-if (isset($_POST['submit'])){
 
+include('config/db_connect.php');
+
+$email = $title = $requirements = '';
+$errors = array('email' => '', 'title' => '', 'requirements' => '');
+
+if(isset($_POST['submit'])){
+
+    // check email
     if(empty($_POST['email'])){
-        $errors['email'] = "Email must be set <br/>";
-    }else{
-    $email = $_POST['email'];
-    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-        $errors['email'] = "Email must be valid <br/>";
-    }
-
-    }
-    if(empty($_POST['title'])){
-        $errors['title'] = "Title must be set<br/>";
-    }else{
-        $title  = $_POST['title'];
-        if(!preg_match('/[a-zA-Z\s]+$/', $title)){
-            $errors['title'] = "Title must be letters and spaces<br/>";
+        $errors['email'] = 'An email is required';
+    } else{
+        $email = $_POST['email'];
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $errors['email'] = 'Email must be a valid email address';
         }
     }
+
+    // check title
+    if(empty($_POST['title'])){
+        $errors['title'] = 'A title is required';
+    } else{
+        $title = $_POST['title'];
+        if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
+            $errors['title'] = 'Title must be letters and spaces only';
+        }
+    }
+
+    // check requirements
     if(empty($_POST['requirements'])){
-        $errors['requirements'] = "Requirements must be set<br/>";
-    }else{
-        $requirements  = $_POST['title'];
-        if(!preg_match('/[a-zA-Z\s]+$/', $requirements)){
-            $errors['requirements'] = "Requirements must be letters and spaces<br/>";
+        $errors['requirements'] = 'At least one requirements is required';
+    } else{
+        $requirements = $_POST['requirements'];
+        if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $requirements)){
+            $errors['requirements'] = 'Requirements must be a comma separated list';
         }
     }
 
     if(array_filter($errors)){
-        echo 'Errors are in the form';
-    }else{
-        echo 'Form is valid';
-        header('Location: index.php');
+        //echo 'errors in form';
+    } else {
+        // escape sql chars
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $title = mysqli_real_escape_string($conn, $_POST['title']);
+        $requirements = mysqli_real_escape_string($conn, $_POST['requirements']);
+
+        // create sql
+        $sql = "INSERT INTO products(title,email,requirements
+) VALUES('$title','$email','$requirements')";
+
+        // save to db and check
+        if(mysqli_query($conn, $sql)){
+            // success
+            header('Location: index.php');
+        } else {
+            echo 'query error: '. mysqli_error($conn);
+        }
+
+
     }
-}
+
+} // end POST check
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
-<?php include('templates/head.php'); ?>
-
-<body class="grey lighten-4">
 <?php include('templates/header.php'); ?>
 
 <section class="container grey-text">
-    <h3 class="center"></h3>
-    <form class="white" action="add.php" method="post">
-        <label for="email">Your Email:</label>
-        <input type="text" id="email" name="email" value="<?php echo htmlspecialchars($email)?>">
-        <div class="red-text"><?php echo $errors['email']?></div>
-        <label for="title">Title:</label>
-        <input type="text" id="title" name="title" value="<?php echo htmlspecialchars($title)?>">
-        <div class="red-text"><?php echo $errors['title']?></div>
-        <label for="requirements">Requirements:</label>
-        <input type="text" id="requirements" name="requirements" value="<?php echo htmlspecialchars($requirements)?>">
-        <div class="red-text"><?php echo $errors['requirements']?></div>
+    <h4 class="center">Add a Product</h4>
+    <form class="white" action="add.php" method="POST">
+        <label>Your Email</label>
+        <input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
+        <div class="red-text"><?php echo $errors['email']; ?></div>
+
+        <label>Product Title</label>
+        <input type="text" name="title" value="<?php echo htmlspecialchars($title) ?>">
+        <div class="red-text"><?php echo $errors['title']; ?></div>
+
+        <label>Requirements (comma separated)</label>
+        <input type="text" name="requirements" value="<?php echo htmlspecialchars($requirements) ?>">
+        <div class="red-text"><?php echo $errors['requirements']; ?></div>
         <div class="center">
-            <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
+            <input type="submit" name="submit" value="Submit" class="btn brand z-depth-0">
         </div>
     </form>
 </section>
 
-
 <?php include('templates/footer.php'); ?>
-</body>
+
 </html>
